a0dc09e2e31b5cee7fb4642c50d9e1439c0761e4
Nick <nicholas.martin2600@gmail.com>
2021-05-29 01:04:02 -0400

    Update npm dependencies, add card images to assets, create ejs views, implement basic card generation with Phaser

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 
 
