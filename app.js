15f5ebf1abc88e0a07d918f1e1ef3362a61ab88b
Nick <nicholas.martin2600@gmail.com>
2021-07-09 23:07:24 -0400

    Add Procfile and prepare codebase for Heroku deployment

--- app.js
+++ app.js
@@ -708,6 +708,11 @@ io.on('connection', (socket) => {
     
 });
 
-server.listen(3000, () => {
-    console.log('Server started on port 3000');
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
